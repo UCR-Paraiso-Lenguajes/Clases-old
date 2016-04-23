10071c9c99b87a7f559812d64a17eda2d42c391b
cguillen <cguillen@testingsoft.com>
2016-04-23 09:11:36 -0600

    Ejemplo controller

--- Lenguajes2/src/main/java/com/ucr/lenguajes/controller/PersonaController.java
+++ Lenguajes2/src/main/java/com/ucr/lenguajes/controller/PersonaController.java
@@ -3,63 +3,70 @@ package com.ucr.lenguajes.controller;
 import java.io.IOException;
 import java.util.HashMap;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.google.gson.Gson;
+import com.ucr.lenguajes.dao.PersonaDao;
 import com.ucr.objectos.Persona;
 @Controller
 public class PersonaController 
 {
 	private int id=0;
 	private HashMap<String, Persona> personas = new HashMap<String, Persona>();
+	@Autowired
+	private PersonaDao personaDao;
 	
 	@RequestMapping(value="/personas", method = RequestMethod.GET)
-	public @ResponseBody String helloWorld5() 
+	public @ResponseBody String obtenerTodos() 
 	{
-		return new Gson().toJson(personas);
+		return new Gson().toJson( personas );
 	}
 	
 	@RequestMapping(value="/personas/{id}", method = RequestMethod.GET)
-	public @ResponseBody String helloWorld9(@PathVariable String id) 
+	public @ResponseBody String obtener(@PathVariable String id) 
 	{
 		return new Gson().toJson(personas.get(id));
 	}
 	
-/*	@RequestMapping(value="/personas/{id}", method = RequestMethod.PUT)
-	public @ResponseBody String helloWorld10(@PathVariable String id) 
+	@RequestMapping(value="/personas/{id}", method = RequestMethod.PUT)
+	public @ResponseBody String actualizar(@PathVariable String id, @RequestBody String body) 
 	{
-		return new Gson().toJson(personas.get(id));
-	}*/
+		Persona persona = new Gson().fromJson(body, Persona.class);
+		
+		personas.put(id, persona);
+		return new Gson().toJson(persona);
+	}
 	
 	@RequestMapping(value="/personas/{id}", method = RequestMethod.DELETE)
-	public void helloWorld11(@PathVariable String id) 
+	public void borrar(@PathVariable String id) 
 	{
 		personas.remove(id);
 	}
 	
 	@RequestMapping(value="/personas", method = RequestMethod.POST)
-	public @ResponseBody String helloWorld4(HttpServletRequest request, HttpServletResponse response) throws IOException
+	public @ResponseBody String crear(@RequestBody String body ) throws IOException
 	{
-		int edad = Integer.parseInt(request.getParameter("edad"));
-		String pais = request.getParameter("pais");
-		
-		Persona p = new Persona(edad, pais);
-		p.setId(""+id);
-		
-		p.setNombre(request.getParameter("nombre"));
-		p.setCedula(request.getParameter("cedula"));
-		personas.put( p.getId(), p );
+		Persona persona = new Gson().fromJson(body, Persona.class);
+		persona.setId(""+id);
+			
+		personas.put( persona.getId() , persona);
 		
 		id++;
 		
-		return new Gson().toJson(p);
+		return new Gson().toJson(persona);
 	}
 	
+	@RequestMapping(value="/personas/sql/{id}", method = RequestMethod.GET)
+	public @ResponseBody String obtenerSql(@PathVariable String id) 
+	{
+		Persona p =personaDao.buscar(id);
+		
+		return new Gson().toJson(p);
+	}
 }
